d723ff7308330c4ccd95629c9cf7a96f74819ed5
Md Sahil <287mdsahil@gmail.com>
2022-02-20 11:50:33 +0530

    toggle term setup

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -100,6 +100,9 @@ return packer.startup(function(use)
   use "moll/vim-bbye"
   use 'nvim-lualine/lualine.nvim'
 
+  -- Terminal
+  use "akinsho/toggleterm.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
